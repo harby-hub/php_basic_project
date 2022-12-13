b53bc920051c2d61146356f3aaeffc147ba671cc
harby <m.harby@quadratechsoft.com>
2022-12-13 17:57:43 +0200

    fix after merge

--- Controllers/Controller.php
+++ Controllers/Controller.php
@@ -21,14 +21,26 @@ class Controller {
         Route( '/article/{slug}/comment/{id<\d+>}' , name: 'article-comment' )
     ]
     public function comment( test $test , int $id , string $slug ) : array {
+        $array = [ 
+            'id'    => [ 1 , 2 ] ,
+            'first' =>   2
+        ];
+        $url = 'https://jsonplaceholder.typicode.com/todos/1' ;
+        $url = 'http://localhost:800/api/v1/test' ;
+        $method = 'get' ;
         return [
-            'url'     => '/article/{slug}/comment/{id<\d+>}' ,
-            'slug'    => $slug                               ,
-            'id'      => $id                                 ,
-            'test'    => $test                               ,
-            'mine'    => $test -> mine                       ,
-            'Wrapper' => $this -> Wrapper                    ,
-            'ENV'     => $_ENV                               ,
+            'url'               => '/article/{slug}/comment/{id<\d+>}'                               ,
+            'slug'              => $slug                                                             ,
+            'id'                => $id                                                               ,
+            'test'              => $test                                                             ,
+            'mine'              => $test -> mine                                                     ,
+            'Wrapper'           => $this -> Wrapper                                                  ,
+            'file_get_contents' => $this -> Wrapper -> file_get_contents ( $method , $url , $array ) ,
+            'fopen'             => $this -> Wrapper -> fopen             ( $method , $url , $array ) ,
+            'curl'              => $this -> Wrapper -> curl              ( $method , $url , $array ) ,
+            'Client'            => $this -> Wrapper -> Client            ( $method , $url , $array ) ,
+            'HttpClient'        => $this -> Wrapper -> HttpClient        ( $method , $url , $array ) ,
+            'ENV'               => $_ENV                               ,
         ] ;
     }
 }
--- Integration/Wrapper.php
+++ Integration/Wrapper.php
@@ -1,6 +1,13 @@
 <?php namespace App\Integration ;
 
 use App\Inject\Parameters;
+use App\Support\Curl;
+
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\ConnectException;
+use Symfony\Component\HttpClient\HttpClient;
+use Symfony\Component\HttpClient\Exception\TransportException;
+
 
 class slave {
     public array $data = [ 1 , 2 , 3 ] ;
@@ -8,5 +15,94 @@ class slave {
 
 #[ Parameters( [ 'slave' => slave::class ] ) ]
 class Wrapper{
+
     public slave $slave ;
+
+    public function fopen( string $method , string $url , array $query = [ ] ) : array {
+        $options = array(
+            'http' => array(
+                'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+                'method'  => strtoupper( $method ) ,
+                'content' => http_build_query( $query )
+            )
+        );
+        $mad = @stream_context_create( $options );
+        $fp  = @fopen( \App\Support\Url::build( $url , $query ) , 'rb' , false , $mad );
+        try {
+            $respone = ( array ) json_decode( stream_get_contents( $fp ) , true ) ;
+            fclose( $fp ) ;
+            return $respone ;
+        } catch ( \Throwable $th ) {
+            return [
+                ( string ) $th -> getMessage( )  ,
+                $th -> getTrace( )  ,
+                ( string ) $th ,
+            ]  ;
+        }
+    }
+
+    public function file_get_contents( string $method , string $url , array $query = [ ] ) : array {
+        $options = array(
+            'http' => array(
+                'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+                'method'  => strtoupper( $method ) ,
+                'content' => http_build_query( $query )
+            )
+        );
+        $result = file_get_contents( \App\Support\Url::build( $url , $query ) , false, stream_context_create( $options ) ) ;
+        try {
+            $respone = ( array ) json_decode( $result , true ) ;
+            return $respone ;
+        } catch ( \Throwable $th ) {
+            return [
+                ( string ) $th -> getMessage( )  ,
+                $th -> getTrace( )  ,
+                ( string ) $th ,
+            ]  ;
+        }
+    }
+
+    public function curl( string $method , string $url , array $query = [ ] ) : array {
+        $ch = Curl::new( )
+            -> url    ( $url    )
+            -> query  ( $query  )
+            -> method ( $method )
+            -> data   ( $query  )
+            -> send   (         )
+            -> content
+        ;
+        return ( array ) json_decode( $ch  , true ) ;
+    }
+
+    public function Client( string $method , string $url , array $query = [ ] ) : array {
+        try{   
+            $response = ( new Client( ) ) -> request( strtoupper( $method ) , $url , [ 'query' => $query ] ) ;
+        } catch ( ConnectException $th) {
+            return [
+                ( string ) $th -> getMessage( )  ,
+                $th -> getTrace( )  ,
+                ( string ) $th ,
+            ]  ;
+        }
+        return ( array ) json_decode( $response -> getBody( ) -> getContents( ) , true ) ;
+    }
+
+    public function HttpClient( string $method , string $url , array $query = [ ] ) : array {
+        try {   
+            return HttpClient::create( [ 'max_redirects' => 3 ] ) -> request( strtoupper( $method ) , $url , [ 'query' => $query ] ) -> toArray( ) ;
+        } catch ( TransportException $th ) {
+            return [
+                ( string ) $th -> getMessage( )  ,
+                $th -> getTrace( )  ,
+                ( string ) $th ,
+            ]  ;
+        } catch ( \Symfony\Component\HttpClient\Exception\JsonException $th ) {
+            return [
+                ( string ) $th -> getMessage( )  ,
+                $th -> getTrace( )  ,
+                ( string ) $th ,
+            ]  ;
+        }
+    }
+
 }
